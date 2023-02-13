25004dfd60d502ea126341eda849c607dd075348
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-13 18:47:23 +0100

    Set up model product class.

---
+++ SuperSold.UI.AspDotNet/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperSold.UI.AspDotNet.Models;
+
+public class Product {
+
+    [Key]
+    public required Guid Id { get; set; }
+    public string UserImgUrl { get; set; } = "";
+
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required decimal Price { get; set; }
+
+}
