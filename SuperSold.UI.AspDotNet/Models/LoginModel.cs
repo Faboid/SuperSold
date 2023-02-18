0d745a7ef7dc1a1ab059bfe2f61040a80c3d87e9
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-18 22:31:05 +0100

    Set up login model and login view.

---
+++ SuperSold.UI.AspDotNet/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperSold.UI.AspDotNet.Models;
+
+public class LoginModel {
+
+    [Required]
+    public required string UserName { get; set; }
+
+    [Required]
+    public required string Password { get; set; }
+
+    public bool RememberMe { get; set; }
+
+}
