f0c84669044681401b7e9ef3e045cba7c34f3c74
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-20 16:23:07 +0100

    Added validation attributes to the login model.

--- SuperSold.UI.AspDotNet/Models/LoginModel.cs
+++ SuperSold.UI.AspDotNet/Models/LoginModel.cs
@@ -4,10 +4,13 @@ namespace SuperSold.UI.AspDotNet.Models;
 
 public class LoginModel {
 
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Must write an username.")]
+    [DataType(DataType.Text)]
     public required string UserName { get; set; }
 
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Must write a password.")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "The password must have at least 8 characters of length.")]
+    [DataType(DataType.Password)]
     public required string Password { get; set; }
 
     public bool RememberMe { get; set; }
