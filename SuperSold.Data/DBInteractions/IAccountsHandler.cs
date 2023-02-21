39f1c57ce9fb2d17af031899106193fad07d8e14
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-21 17:40:41 +0100

    Implemented interfaces to interface with the database.

---
+++ SuperSold.Data/DBInteractions/IAccountsHandler.cs
@@ -0,0 +1,13 @@
+﻿using Monads.Core;
+using SuperSold.Data.Models;
+
+namespace SuperSold.Data.DBInteractions;
+public interface IAccountsHandler {
+
+    Task<Option<AccountModel>> GetAccountByUserName(string accountName);
+    Task<bool> RenameAccount(string accountName, string newName);
+    Task<bool> ChangeEmail(string accountName, string newEmail);
+    Task<bool> DeleteAccount(string accountName);
+    Task<bool> CreateAccount(AccountModel model);
+
+}
---
+++ SuperSold.Data/DBInteractions/IProductsHandler.cs
@@ -0,0 +1,12 @@
+﻿using SuperSold.Data.Models;
+
+namespace SuperSold.Data.DBInteractions;
+internal interface IProductsHandler {
+
+    Task<IQueryable<ProductModel>> QueryAllProducts();
+    Task<IQueryable<ProductModel>> QueryProductsBySellerUserName(string userName);
+    Task<bool> EditProduct(Guid productId, ProductModel updatedValues);
+    Task<bool> DeleteProduct(Guid productId);
+    Task<bool> CreateProduct(ProductModel product, string sellerUserName);
+
+}
