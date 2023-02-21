b818143d44c356a271126ca63678c7e64d1c08d6
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-21 17:40:25 +0100

    Implemented models that mimic the database table structure.

---
+++ SuperSold.Data/Models/AccountModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperSold.Data.Models;
+
+public class AccountModel {
+
+    public Guid? IdAccount { get; set; }
+    public required string UserName { get; set; }
+    public required string Email { get; set; }
+    public required string HashedPassword { get; set; }
+
+}
---
+++ SuperSold.Data/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperSold.Data.Models;
+
+public class ProductModel {
+
+    public Guid IdProduct { get; set; }
+    public Guid IdSellerAccount { get; set; }
+    public string? ImageUrl { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required decimal Price { get; set; }
+
+}
+
