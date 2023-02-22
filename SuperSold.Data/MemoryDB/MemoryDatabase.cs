a0ede9453966100b3448dac4afc0fd297bd97940
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-22 16:50:16 +0100

    Implemented memory-based cache database for testing purposes.

---
+++ SuperSold.Data/MemoryDB/MemoryDatabase.cs
@@ -0,0 +1,18 @@
+﻿using SuperSold.Data.Models;
+
+namespace SuperSold.Data.MemoryDB;
+
+/// <summary>
+/// Mocks a database. Does not retain information between sessions—use only for test purposes.
+/// </summary>
+public class MemoryDatabase {
+
+    public MemoryDatabase() : this(new(), new()) { }
+    public MemoryDatabase(Dictionary<string, AccountModel> accountsTable, Dictionary<Guid, ProductModel> productsTable) {
+        AccountsTable = accountsTable;
+        ProductsTable = productsTable;
+    }
+
+    public Dictionary<string, AccountModel> AccountsTable { get; init; }
+    public Dictionary<Guid, ProductModel> ProductsTable { get; init; }
+}
