690c6a4dafde25f62052e72ebc98cdee104dde63
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-24 20:12:36 +0100

    Set up usage of IProductsHandler to obtain user-specific posted items.

--- SuperSold.UI.AspDotNet/Controllers/ProductsController.cs
+++ SuperSold.UI.AspDotNet/Controllers/ProductsController.cs
@@ -1,24 +1,27 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SuperSold.Data.DBInteractions;
+using SuperSold.UI.AspDotNet.Extensions;
 using SuperSold.UI.AspDotNet.Models;
 
 namespace SuperSold.UI.AspDotNet.Controllers;
 public class ProductsController : Controller {
 
+    private readonly IProductsHandler _productsHandler;
+
+    public ProductsController(IProductsHandler productsHandler) {
+        _productsHandler = productsHandler;
+    }
+
     [HttpGet]
     [Authorize]
-    public IActionResult MyProducts() {
+    public async Task<IActionResult> MyProducts(int? row) {
 
-        if(User.Identity!.Name != "admin") {
-            return View(new List<Product>());
-        }
-
-        var listProducts = new List<Product> {
-            new() { Id = Guid.NewGuid(), Title = "Something nice", Description = "A nice description", Price = 10, UserImgUrl = "" },
-            new() { Id = Guid.NewGuid(), Title = "Xbox", Description = "Used xbox with minor scratches.", Price = 280.32M, UserImgUrl = "" }
-        };
-
+        var userName = User.Identity!.Name!;
+        var products = await _productsHandler.QueryProductsBySellerUserName(userName);
+        var listProducts = await products.Select(x => (Product)x).Skip((row ?? 0) * 10).Take(10).ToListAsyncSafe();
         return View(listProducts);
+
     }
 
 }
