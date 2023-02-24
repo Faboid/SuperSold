f495fcffd2a2923e6a4bd223d1d7c26068771865
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-24 20:10:13 +0100

    Implemented IQueryable extension to safely convert it to a list even if the underlying type does not support asynchonous behavior.

---
+++ SuperSold.UI.AspDotNet/Extensions/QueryableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperSold.UI.AspDotNet.Extensions;
+
+public static class QueryableExtensions {
+
+    /// <summary>
+    /// Used to enumerate the queryable into a list.
+    /// <br/>Checks if the <see cref="IQueryable{T}"/> implements <see cref="IAsyncEnumerable{T}"/>. 
+    /// If it doesn't, it fall-back to the synchronous <see cref="IQueryable{T}.ToList()"/> with a <see cref="Task.FromResult{TResult}(TResult)"/> wrapper.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="queryable"></param>
+    /// <returns></returns>
+    public static Task<List<T>> ToListAsyncSafe<T>(this IQueryable<T> queryable) {
+
+        ArgumentNullException.ThrowIfNull(queryable);
+
+        if(queryable is not IAsyncEnumerable<T>) {
+            return Task.FromResult(queryable.ToList());
+        }
+
+        return queryable.ToListAsync();
+
+    }
+
+}
