21ee26ea21e713083b94804c9ceb66cba76cc8b6
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-25 16:17:07 +0100

    Toggled nullability of IdAccount in the AccountModel.

--- SuperSold.Data/Models/AccountModel.cs
+++ SuperSold.Data/Models/AccountModel.cs
@@ -2,7 +2,7 @@
 
 public class AccountModel {
 
-    public Guid? IdAccount { get; set; }
+    public Guid IdAccount { get; set; }
     public required string UserName { get; set; }
     public required string Email { get; set; }
     public required string HashedPassword { get; set; }
