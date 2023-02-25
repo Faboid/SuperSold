ba3a3bf0e7ceeaf53de55584e4e4eacd3c2a0af5
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-25 14:13:37 +0100

    Created authentication-centric assembly.

---
+++ SuperSold.UI.AspDotNet/HostBuilders/AddAuthenticationHelpersHostBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SuperSold.Identification;
+
+namespace SuperSold.UI.AspDotNet.HostBuilders;
+
+public static class AddAuthenticationHelpersHostBuilderExtensions {
+
+    public static void AddAuthenticationHelpers(this IServiceCollection services) {
+
+        services.AddSingleton<IAuthenticator, Authenticator>();
+
+    }
+
+}
