a2a8b36e787103fdc950b907eb725749b758deed
Faboid <72757782+Faboid@users.noreply.github.com>
2023-02-25 16:21:01 +0100

    Implemented API point to publish a new product.

--- SuperSold.UI.AspDotNet/Controllers/ProductsController.cs
+++ SuperSold.UI.AspDotNet/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SuperSold.Data.DBInteractions;
 using SuperSold.UI.AspDotNet.Extensions;
 using SuperSold.UI.AspDotNet.Models;
+using System.Security.Claims;
 
 namespace SuperSold.UI.AspDotNet.Controllers;
 public class ProductsController : Controller {
@@ -27,6 +28,37 @@ public class ProductsController : Controller {
             .ToListAsyncSafe();
 
         return View(listProducts);
+
+    }
+
+    [HttpGet]
+    [Authorize]
+    public IActionResult Publish() => View();
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Publish(Product product) {
+
+        if(!ModelState.IsValid) {
+            return View();
+        }
+
+        var user = User.Identity!.Name!;
+        var sellerId = User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+        product.Id = Guid.NewGuid();
+        product.SellerId = Guid.Parse(sellerId);
+
+        var result = await _productsHandler.CreateProduct(product, user);
+
+        return result.Match<IActionResult>(
+            success => Redirect("MyProducts"),
+            alreadyExists => {
+                ViewBag.Message = "There has been a duplication error. It's suggested to reload the page and re-enter the values.";
+                //todo - this should never happen. Add logging and consider returning a 500 error
+                return View();
+            }
+        );
 
     }
 
