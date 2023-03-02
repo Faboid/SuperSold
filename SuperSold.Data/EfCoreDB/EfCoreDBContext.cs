20ffe959207db4791fdbf3b8ff6759e02910a2ae
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-02 17:36:52 +0100

    Set up EFCore-related DB repository layer.

---
+++ SuperSold.Data/EfCoreDB/EfCoreDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperSold.Data.Models;
+
+namespace SuperSold.Data.EfCoreDB;
+
+public class EfCoreDBContext : DbContext {
+
+    public DbSet<AccountModel> Accounts { get; set; }
+    public DbSet<ProductModel> Products { get; set; }
+
+    public EfCoreDBContext(DbContextOptions<EfCoreDBContext> options) : base(options) { }
+
+}
+
