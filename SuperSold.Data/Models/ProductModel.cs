9496c833aca6c49ae97de0d84b7e452ca96e955a
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-03 17:22:11 +0100

    Set up key-related attributes.

--- SuperSold.Data/Models/ProductModel.cs
+++ SuperSold.Data/Models/ProductModel.cs
@@ -1,9 +1,16 @@
-﻿namespace SuperSold.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuperSold.Data.Models;
 
 public class ProductModel {
 
+    [Key]
     public Guid IdProduct { get; set; }
+    
+    [ForeignKey(nameof(AccountModel.IdAccount))]
     public Guid IdSellerAccount { get; set; }
+
     public string? ImageUrl { get; set; }
     public required string Title { get; set; }
     public required string Description { get; set; }
