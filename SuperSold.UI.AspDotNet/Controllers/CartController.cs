5f0727b2c0da5cc4918779b0de7d804842c621b7
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-03 19:41:05 +0100

    Created CartController, to handle the cart, wishlist, and the buying of items.

---
+++ SuperSold.UI.AspDotNet/Controllers/CartController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SuperSold.Data.DBInteractions;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.Controllers;
+
+[Authorize]
+[AutoValidateAntiforgeryToken]
+public class CartController : Controller {
+
+    private readonly IProductsHandler _productsHandler;
+
+    public CartController(IProductsHandler productsHandler) {
+        _productsHandler = productsHandler;
+    }
+
+    [HttpGet]
+    public IActionResult WishList(int? page = 0) {
+        return View();
+    }
+
+    [HttpGet]
+    [Route("/Cart/View")]
+    public IActionResult ViewCart(int? page = 0) {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Buy() {
+        return View();
+    }
+
+    [HttpPost]
+    public Task<IActionResult> AddToCart(Product product) {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public Task<IActionResult> AddToWishlist(Product product) {
+        throw new NotImplementedException();
+    }
+
+}
