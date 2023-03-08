47b966559f3d1b309881f75dab032b3f4bdc2465
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-08 20:11:30 +0100

    (WIP)Created scrollableview, a modem to handle the generation of infinitely scrollable views.

---
+++ SuperSold.UI.AspDotNet/Models/ScrollableViewModel.cs
@@ -0,0 +1,27 @@
+﻿using SuperSold.UI.AspDotNet.Extensions;
+
+namespace SuperSold.UI.AspDotNet.Models;
+
+public class ScrollableViewModel<T> {
+
+    public ScrollableViewModel(IQueryable<T> modelList) {
+        ModelList = modelList;
+    }
+
+    public IQueryable<T> ModelList { get; set; }
+    public string RowFormatPartialViewName { get; set; } = "_BuyableProductRowPartial"; //temp hardcoded
+
+    public int Cursor { get; set; } = 0;
+    public async Task<List<T>> NextAsync(int amount) {
+
+        var output = await ModelList
+            .Skip(Cursor)
+            .Take(amount)
+            .ToListAsyncSafe();
+
+        Cursor += amount;
+        return output;
+
+    }
+
+}
