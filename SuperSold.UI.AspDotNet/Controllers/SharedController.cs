c10491a79b91b68254b4ca66b0be0ecf10be6658
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-08 20:13:41 +0100

    Created controller to return partial views.

---
+++ SuperSold.UI.AspDotNet/Controllers/SharedController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.Controllers;
+public class SharedController : Controller {
+    
+    public IActionResult RenderProductList(string rowPartialName, ICollection<Product> products) {
+        return PartialView("_ProductListPartialView", (rowPartialName, products));
+    }
+
+}
