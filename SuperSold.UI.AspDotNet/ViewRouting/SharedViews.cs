e9e3269feaba8627e77373fcb2538879353ddb77
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-09 18:25:15 +0100

    Set up a more type-safe way of calling views. (WIP - only set up for the productpartialview.)

---
+++ SuperSold.UI.AspDotNet/ViewRouting/SharedViews.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.ViewRouting;
+
+public static class SharedViews {
+
+    public static IActionResult ProductListPartialView(this Controller controller, string rowPartialName, IEnumerable<Product> products) {
+        return controller.PartialView("_ProductListPartialView", (rowPartialName, products));
+    }
+
+}
