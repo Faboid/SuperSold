719b5d8ea7b71f21deee39b3faab5bdbdccee5cd
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-10 16:36:34 +0100

    Implemented method to generate scrollable page.

---
+++ SuperSold.UI.AspDotNet/ViewRouting/Scrollable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.ViewRouting;
+
+public static class Scrollable {
+
+    public static async Task<IHtmlContent> ScrollablePageOnly<T>(this IHtmlHelper<T> htmlHelper, string url) {
+        return await htmlHelper.PartialAsync("ScrollableViewCollection", new ScrollableViewModel(url));
+    }
+
+    public static Task<IHtmlContent> ScrollablePageSearch(string search) {
+        throw new NotImplementedException();
+    }
+
+}
