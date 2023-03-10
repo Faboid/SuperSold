fc1893d4c780f78607f378481718df6b16c58698
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-10 17:41:48 +0100

    Implemented additional method to handle requests that inclue search a parameter.

--- SuperSold.UI.AspDotNet/wwwroot/js/productLoad.js
+++ SuperSold.UI.AspDotNet/wwwroot/js/productLoad.js
@@ -2,12 +2,31 @@
 let page = 0;
 const logMessage = "Add additional scrollable page elements.";
 
-function loadMore(url, scrollContainer) {
+function loadMorePageOnly(url, scrollContainer) {
 
     $.ajax({
         url: url,
         type: 'GET',
         data: { "page": page },
+        dataType: "html",
+        cache: false,
+    }).done(function (result) {
+        console.log("Success: " + logMessage);
+        scrollContainer.innerHTML += result;
+        page++;
+    }).fail(function (error) {
+        console.log("Failed: " + logMessage);
+        console.log(error);
+    });
+
+}
+
+function loadMoreWithSearch(url, search, scrollContainer) {
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: { "page": page, "search": search },
         dataType: "html",
         cache: false,
     }).done(function (result) {
