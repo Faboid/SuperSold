a5f464bc2b4ae7a5e878c00514659128cf7349b8
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-10 17:42:52 +0100

    Added a search parameter to the scrollableviewmodel.

--- SuperSold.UI.AspDotNet/Models/ScrollableViewModel.cs
+++ SuperSold.UI.AspDotNet/Models/ScrollableViewModel.cs
@@ -4,10 +4,12 @@ namespace SuperSold.UI.AspDotNet.Models;
 
 public class ScrollableViewModel {
 
-    public ScrollableViewModel(string url) {
+    public ScrollableViewModel(string url, string? search = null) {
         Url = url;
+        SearchItem = search;
     }
 
     public string Url { get; set; }
+    public string? SearchItem { get; set; }
 
 }
