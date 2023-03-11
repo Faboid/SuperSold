65828e9408972f6cef2a31e7f867e893ad141a43
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-11 15:24:57 +0100

    Added const string to product row partial's view name.

--- SuperSold.UI.AspDotNet/ViewRouting/PartialViewNames.cs
+++ SuperSold.UI.AspDotNet/ViewRouting/PartialViewNames.cs
@@ -3,5 +3,6 @@
 public static class PartialViewNames {
 
     public const string BuyableProductRowPartial = "_BuyableProductRowPartial";
+    public const string ProductRowPartial = "_ProductRowPartial";
 
 }
