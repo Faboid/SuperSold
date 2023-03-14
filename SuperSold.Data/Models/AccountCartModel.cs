d22e125a204e190bb1ccaa74eb970d15a9fd0014
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-14 19:04:43 +0100

    Set up models.

--- SuperSold.Data/Models/AccountCartModel.cs
+++ SuperSold.Data/Models/AccountCartModel.cs
@@ -1,8 +1,15 @@
-﻿namespace SuperSold.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace SuperSold.Data.Models;
+
+[Keyless]
 public class AccountCartModel {
 
+    [ForeignKey(nameof(AccountModel.IdAccount))]
     public Guid AccountId { get; set; }
+
+    [ForeignKey(nameof(ProductModel.IdProduct))]
     public Guid ProductId { get; set; }
 
 }
--- SuperSold.Data/Models/AccountWishlistModel.cs
+++ SuperSold.Data/Models/AccountWishlistModel.cs
@@ -1,8 +1,15 @@
-﻿namespace SuperSold.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace SuperSold.Data.Models;
+
+[Keyless]
 public class AccountWishlistModel {
 
+    [ForeignKey(nameof(AccountModel.IdAccount))]
     public Guid AccountId { get; set; }
+    
+    [ForeignKey(nameof(ProductModel.IdProduct))]
     public Guid ProductId { get; set; }
 
 }
