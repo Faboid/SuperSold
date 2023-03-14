cbd2af8da9808265744baf863a1b792925f7d4d1
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-14 19:07:17 +0100

    Set up extra row models to handle additional view types.

--- SuperSold.UI.AspDotNet/ViewRouting/PartialViewNames.cs
+++ SuperSold.UI.AspDotNet/ViewRouting/PartialViewNames.cs
@@ -3,6 +3,8 @@
 public static class PartialViewNames {
 
     public const string BuyableProductRowPartial = "_BuyableProductRowPartial";
+    public const string CartProductRowPartial = "_CartProductRowPartial";
+    public const string WishlistProductRowPartial = "_WishlistProductRowPartial";
     public const string ProductRowPartial = "_ProductRowPartial";
 
 }
