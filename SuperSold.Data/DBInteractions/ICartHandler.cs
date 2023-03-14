40ae650c0e2fa54493bfe6a1f485c2395a368545
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-14 19:04:58 +0100

    Set up interfaces to interact with the DB.

---
+++ SuperSold.Data/DBInteractions/ICartHandler.cs
@@ -0,0 +1,14 @@
+﻿using OneOf;
+using OneOf.Types;
+using SuperSold.Data.Models;
+using SuperSold.Data.Models.ResponseTypes;
+
+namespace SuperSold.Data.DBInteractions;
+
+public interface ICartHandler {
+
+    IQueryable<ProductModel> QueryCartedProductsByUserId(Guid userId);
+    Task<OneOf<Success, AlreadyExists>> AddToCart(Guid userId, Guid productId);
+    Task<OneOf<Success, NotFound>> RemoveFromCart(Guid userId, Guid productId);
+
+}
---
+++ SuperSold.Data/DBInteractions/IWishlistHandler.cs
@@ -0,0 +1,12 @@
+﻿using OneOf;
+using OneOf.Types;
+using SuperSold.Data.Models;
+
+namespace SuperSold.Data.DBInteractions;
+public interface IWishlistHandler {
+
+    IQueryable<ProductModel> QueryWishlistedProductsByUserId(Guid userId);
+    Task<OneOf<Success, Error>> WishlistProduct(Guid userId, Guid productId);
+    Task<OneOf<Success, NotFound>> RemoveWishlistProduct(Guid userId, Guid productId);
+
+}
