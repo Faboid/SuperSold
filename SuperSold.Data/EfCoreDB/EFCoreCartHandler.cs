0e0e575bd08911565591c9a3e819855b612eff78
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-14 19:06:23 +0100

    Implemented repository classes to interact with wishlist and cart tables.

---
+++ SuperSold.Data/EfCoreDB/EFCoreCartHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OneOf;
+using OneOf.Types;
+using SuperSold.Data.DBInteractions;
+using SuperSold.Data.Models;
+using SuperSold.Data.Models.ResponseTypes;
+
+namespace SuperSold.Data.EfCoreDB;
+
+public class EFCoreCartHandler : ICartHandler {
+
+    private readonly EfCoreDBContext _context;
+
+    public EFCoreCartHandler(EfCoreDBContext context) {
+        _context = context;
+    }
+
+    public IQueryable<ProductModel> QueryCartedProductsByUserId(Guid userId) {
+        var products = _context.Products
+            .AsNoTracking()
+            .GroupJoin(_context.Cart.Where(x => x.AccountId == userId), x => x.IdProduct, x => x.ProductId, (x, y) => x);
+
+        return products;
+    }
+
+    public async Task<OneOf<Success, AlreadyExists>> AddToCart(Guid userId, Guid productId) {
+
+        var cartItem = new AccountCartModel() { AccountId = userId, ProductId = productId };
+
+        //check if this item is already in the cart
+        if(await _context.Cart.ContainsAsync(cartItem)) {
+            return new AlreadyExists();
+        }
+
+        await _context.Cart.AddAsync(cartItem);
+        await _context.SaveChangesAsync();
+        return new Success();
+
+    }
+
+    public async Task<OneOf<Success, NotFound>> RemoveFromCart(Guid userId, Guid productId) {
+
+        var cartItem = await _context.Cart.FirstOrDefaultAsync(x => x.AccountId == userId && x.ProductId == productId);
+        if(cartItem is null) {
+            return new NotFound();
+        }
+
+        _context.Cart.Remove(cartItem);
+        await _context.SaveChangesAsync();
+        return new Success();
+
+    }
+}
---
+++ SuperSold.Data/EfCoreDB/EfCoreWishlistHandler.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OneOf;
+using OneOf.Types;
+using SuperSold.Data.DBInteractions;
+using SuperSold.Data.Models;
+
+namespace SuperSold.Data.EfCoreDB;
+public class EfCoreWishlistHandler : IWishlistHandler {
+
+    private readonly EfCoreDBContext _context;
+
+    public EfCoreWishlistHandler(EfCoreDBContext context) {
+        _context = context;
+    }
+
+    public IQueryable<ProductModel> QueryWishlistedProductsByUserId(Guid userId) {
+
+        var products = _context.Products
+            .AsNoTracking()
+            .GroupJoin(_context.Wishlists.Where(x => x.AccountId == userId), x => x.IdProduct, x => x.ProductId, (x, y) => x);
+
+        return products;
+
+    }
+
+    public async Task<OneOf<Success, NotFound>> RemoveWishlistProduct(Guid userId, Guid productId) {
+        
+        var wishlist = await _context.Wishlists.FirstOrDefaultAsync(x => x.AccountId == userId && x.ProductId == productId);
+        if(wishlist is null) {
+            return new NotFound();
+        }
+
+        _context.Wishlists.Remove(wishlist);
+        await _context.SaveChangesAsync();
+        return new Success();
+
+    }
+
+    public async Task<OneOf<Success, Error>> WishlistProduct(Guid userId, Guid productId) {
+
+        var wishlist = new AccountWishlistModel() { AccountId = userId, ProductId = productId };
+        await _context.Wishlists.AddAsync(wishlist);
+        var result = await _context.SaveChangesAsync();
+
+        if(result == 1) {
+            return new Success();
+        } else {
+            return new Error();
+        }
+
+    }
+
+
+}
