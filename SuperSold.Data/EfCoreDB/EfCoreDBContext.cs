e4fcb5fc0ead8c49b92388ecb604d5d724086f2e
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-14 19:05:44 +0100

    Added wishlist and cart tables to dbcontext.

--- SuperSold.Data/EfCoreDB/EfCoreDBContext.cs
+++ SuperSold.Data/EfCoreDB/EfCoreDBContext.cs
@@ -7,8 +7,13 @@ public class EfCoreDBContext : DbContext {
 
     public DbSet<AccountModel> Accounts { get; set; }
     public DbSet<ProductModel> Products { get; set; }
+    public DbSet<AccountWishlistModel> Wishlists { get; set; }
+    public DbSet<AccountCartModel> Cart { get; set; }
 
     public EfCoreDBContext(DbContextOptions<EfCoreDBContext> options) : base(options) { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+    }
 }
 
