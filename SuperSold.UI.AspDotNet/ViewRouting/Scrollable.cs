f9e71038a3e37a8154d4fcebf37cd3f84a825b5e
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-20 20:05:23 +0100

    Set up customizable scrollable id.

--- SuperSold.UI.AspDotNet/ViewRouting/Scrollable.cs
+++ SuperSold.UI.AspDotNet/ViewRouting/Scrollable.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using OneOf;
 using SuperSold.UI.AspDotNet.Models;
 
 namespace SuperSold.UI.AspDotNet.ViewRouting;
 
 public static class Scrollable {
 
-    public static async Task<IHtmlContent> ScrollablePartialView<T>(this IHtmlHelper<T> htmlHelper, string url) {
-        return await htmlHelper.PartialAsync("_ScrollablePageOnlyPartialView", new ScrollableViewModel(url));
+    public static async Task<IHtmlContent> ScrollablePartialView<T>(this IHtmlHelper<T> htmlHelper, string url, OneOf<Guid, string>? customId = null) {
+        return await htmlHelper.PartialAsync("_ScrollablePageOnlyPartialView", new ScrollableViewModel(url, customId ?? Guid.NewGuid()));
     }
 
-    public static async Task<IHtmlContent> ScrollablePartialView<T>(this IHtmlHelper<T> htmlHelper, string url, string search) {
-        return await htmlHelper.PartialAsync("_ScrollablePageSearchPartialView", new ScrollableViewModel(url, search));
+    public static async Task<IHtmlContent> ScrollablePartialView<T>(this IHtmlHelper<T> htmlHelper, string url, string search, OneOf<Guid, string>? customId = null) {
+        return await htmlHelper.PartialAsync("_ScrollablePageSearchPartialView", new ScrollableViewModel(url, customId ?? Guid.NewGuid(), search));
     }
 
 }
