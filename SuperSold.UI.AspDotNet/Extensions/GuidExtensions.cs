1e3ab364f30be8785343a69d75afec010f30fedd
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-23 17:02:29 +0100

    Set up scrollable ids.

---
+++ SuperSold.UI.AspDotNet/Extensions/GuidExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace SuperSold.UI.AspDotNet.Extensions;
+
+public static class GuidExtensions {
+
+    public static string ToValidHtml(this Guid guid) => guid.ToString().Replace('-', 'a');
+
+}
