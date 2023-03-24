aef13c7f99bc392071fa87177f80782798803996
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-24 17:39:06 +0100

    Implemented shared method to send a message to the user. This abstraction will allow later changing to a less invasiv method than "alert".

--- SuperSold.UI.AspDotNet/wwwroot/js/site.js
+++ SuperSold.UI.AspDotNet/wwwroot/js/site.js
@@ -1,4 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+//sends a message to the user. Currently uses alert, but will implement a partial view in the future
+function notifyUser(message) {
+    alert(message);
+}
