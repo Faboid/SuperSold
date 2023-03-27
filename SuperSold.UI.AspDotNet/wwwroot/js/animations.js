880b229f34530965b96c4406aa279031e3379d0d
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-27 18:17:55 +0200

    Set up deletion of element on pullUp. Might have to rename this function.

--- SuperSold.UI.AspDotNet/wwwroot/js/animations.js
+++ SuperSold.UI.AspDotNet/wwwroot/js/animations.js
@@ -1,5 +1,9 @@
 ﻿
 
 function pullUp(element) {
-    element.animate({ height: 'toggle', opacity: '0%' });
+    element.animate({ height: 'toggle', opacity: '0%' }, function () {
+        //delete element afterward
+        //todo - rename pullUp to better reflect its behavior
+        element.remove();
+    });
 }
