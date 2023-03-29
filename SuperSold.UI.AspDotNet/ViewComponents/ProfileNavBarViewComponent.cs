0c13ed59adafbff4c9fac268bca1dbe788ed9d98
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-29 15:32:46 +0200

    Moved login-related buttons to view component to provide better segregation.

---
+++ SuperSold.UI.AspDotNet/ViewComponents/ProfileNavBarViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperSold.UI.AspDotNet.ViewComponents;
+
+public class ProfileNavBarViewComponent : ViewComponent {
+
+    public IViewComponentResult Invoke() {
+        
+        if(!User.Identity!.IsAuthenticated) {
+            return View("Login");
+        }
+
+        return View("Default");
+
+    }
+
+}
