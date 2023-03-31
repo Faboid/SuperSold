310cb38e474eb83407c7bde84f6241b4044a63f5
Faboid <72757782+Faboid@users.noreply.github.com>
2023-03-31 19:01:58 +0200

    Created Profile/Index route & view.

---
+++ SuperSold.UI.AspDotNet/Controllers/ProfileController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperSold.UI.AspDotNet.Controllers;
+public class ProfileController : Controller {
+    public IActionResult Index() {
+        return View();
+    }
+}
--- SuperSold.UI.AspDotNet/ViewRouting/RoutesBuilder.cs
+++ SuperSold.UI.AspDotNet/ViewRouting/RoutesBuilder.cs
@@ -5,10 +5,23 @@ namespace SuperSold.UI.AspDotNet.ViewRouting;
 
 public static class RoutesBuilder {
 
+    public static ProfileRoutes Profile(this IUrlHelper urlHelper) => new(urlHelper);
     public static CartRoutes Cart(this IUrlHelper urlHelper) => new(urlHelper);
     public static WishlistRoutes Wishlist(this IUrlHelper urlHelper) => new(urlHelper);
     public static AuthenticationRoutes Authentication(this IUrlHelper urlHelper) => new(urlHelper);
     public static HomeRoutes Home(this IUrlHelper urlHelper) => new(urlHelper);
     public static ProductsRoutes Products(this IUrlHelper urlHelper) => new(urlHelper);
+
+}
+
+public class ProfileRoutes {
+
+    private readonly IUrlHelper _urlHelper;
+
+    public ProfileRoutes(IUrlHelper urlHelper) {
+        _urlHelper = urlHelper;
+    }
+
+    public string? Index() => _urlHelper.Action("Index", "Profile");
 
 }
