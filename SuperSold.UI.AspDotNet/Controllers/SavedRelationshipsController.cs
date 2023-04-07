625fedbf154307b2918dd9a4ebb8ebea4344c8be
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-07 16:37:44 +0200

    Set up endpoint to modify a saved relationship's quantity.

---
+++ SuperSold.UI.AspDotNet/Controllers/SavedRelationshipsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperSold.Data.DBInteractions;
+
+namespace SuperSold.UI.AspDotNet.Controllers;
+public class SavedRelationshipsController : Controller {
+
+    private readonly ISavedRelationshipsHandler _savedRelationshipsHandler;
+
+    public SavedRelationshipsController(ISavedRelationshipsHandler savedRelationshipsHandler) {
+        _savedRelationshipsHandler = savedRelationshipsHandler;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ModifyQuantity(Guid relationshipId, int quantity) {
+
+        var result = await _savedRelationshipsHandler.UpdateRelationshipQuantity(relationshipId, quantity);
+        return result.Match<IActionResult>(
+            success => Ok(),
+            notfound => NotFound()
+        );
+
+    } 
+
+}
