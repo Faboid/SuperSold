f0a4d2d4012da154a9ae4683aadf29971a4f2371
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-13 19:26:08 +0200

    Set up views and partial views for the purchase page.

---
+++ SuperSold.UI.AspDotNet/ViewRouting/PartialViews/PartialBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SuperSold.UI.AspDotNet.ViewRouting.PartialViews;
+
+public static class PartialBuilder {
+
+    public static PartialPurchaseRoutes PartialPurchase(this IHtmlHelper htmlHelper) => new(htmlHelper);
+
+}
---
+++ SuperSold.UI.AspDotNet/ViewRouting/PartialViews/PartialPurchaseRoutes.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SuperSold.UI.AspDotNet.ViewRouting.PartialViews;
+
+public class PartialPurchaseRoutes {
+
+    private readonly IHtmlHelper _htmlHelper;
+
+    public PartialPurchaseRoutes(IHtmlHelper htmlHelper) {
+        _htmlHelper = htmlHelper;
+    }
+
+    public async Task<IHtmlContent> RenderFinalConfirmationView() => await _htmlHelper.PartialAsync("_FinalConfirmation.cshtml");
+    public async Task<IHtmlContent> RenderCreditCardRequestView() => await _htmlHelper.PartialAsync("_CreditCardInformationRequest.cshtml");
+    public async Task<IHtmlContent> RenderShippingInformationRequestView() => await _htmlHelper.PartialAsync("_ShippingInformationRequest.cshtml");
+
+}
