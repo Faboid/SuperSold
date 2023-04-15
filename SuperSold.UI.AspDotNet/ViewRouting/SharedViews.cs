955d8eff121f5d8dcdd7125fbbf3de92200f763f
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-15 17:59:59 +0200

    Implemented list of products for the purchase view.

--- SuperSold.UI.AspDotNet/ViewRouting/SharedViews.cs
+++ SuperSold.UI.AspDotNet/ViewRouting/SharedViews.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SuperSold.Data.Models;
 using SuperSold.UI.AspDotNet.Models;
 
@@ -16,6 +18,10 @@ public static class SharedViews {
 
     public static IActionResult RelationshipListPartialView(this Controller controller, string rowType, IEnumerable<ProductWithSavedRelationship> products) {
         return controller.PartialView("_RelationshipListPartialView", (rowType, products));
+    }
+
+    public static async Task<IHtmlContent> RelationshipListPartialView(this IHtmlHelper html, string rowType, IEnumerable<ProductWithSavedRelationship> products) {
+        return await html.PartialAsync("_RelationshipListPartialView", (rowType, products));
     }
 
     public static IActionResult RelationshipListPartialView(this Controller controller, string rowType, ProductWithSavedRelationship product) {
