9c7cebc813e06d21f7a4f6e063e48084c2fbd35b
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-15 18:01:42 +0200

    Removed unused partial views.

--- SuperSold.UI.AspDotNet/ViewRouting/PartialViews/PartialBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-
-namespace SuperSold.UI.AspDotNet.ViewRouting.PartialViews;
-
-public static class PartialBuilder {
-
-    public static PartialPurchaseRoutes PartialPurchase(this IHtmlHelper htmlHelper) => new(htmlHelper);
-
-}
