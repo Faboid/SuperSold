4859f36b27cbcc034053a549d54c63ef491c2987
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-17 17:43:15 +0200

    Added "price" parameter and built a response for submitting the purchase action. As no money is being moved, there is no need to implement a payment-related class to use a third party bank api library.

--- SuperSold.UI.AspDotNet/Controllers/PurchaseController.cs
+++ SuperSold.UI.AspDotNet/Controllers/PurchaseController.cs
@@ -12,12 +12,14 @@ namespace SuperSold.UI.AspDotNet.Controllers;
 [AutoValidateAntiforgeryToken]
 public class PurchaseController : Controller {
 
+    private readonly IProductsHandler _productsHandler;
     private readonly ICartHandler _cartHandler;
     private readonly IMapper _mapper;
 
-    public PurchaseController(ICartHandler cartHandler, IMapper mapper) {
+    public PurchaseController(ICartHandler cartHandler, IMapper mapper, IProductsHandler productsHandler) {
         _cartHandler = cartHandler;
         _mapper = mapper;
+        _productsHandler = productsHandler;
     }
 
     public async Task<IActionResult> Index() {
@@ -32,11 +34,34 @@ public class PurchaseController : Controller {
     }
 
     [HttpPost]
-    public Task<IActionResult> SubmitPurchase(PurchaseInfoModel info, string products) {
+    public async Task<IActionResult> SubmitPurchase(PurchaseInfoModel info, decimal price, string products) {
 
-        var objProducts = products.Split(',').Select(ProductWithSavedRelationship.ParseBasicString).ToList();
+        if(!info.WarningCheckbox) {
+            return BadRequest("Must tick the disclaimer checkbox.");
+        }
 
-        throw new NotImplementedException();
+        var objProducts = products
+            .Split(',')
+            .Select(ProductWithSavedRelationship.ParseBasicString)
+            .ToList();
+
+        var oneofProducts = await objProducts
+            .Select(async x => await _productsHandler.GetProduct(x.guid))
+            .ToListAsync();
+
+        if(oneofProducts.Any(x => x.IsT1)) {
+            return StatusCode(412, "One or more of the products in the cart is not on sale anymore.");
+        }
+
+        var dbProducts = oneofProducts.Select(x => x.AsT0).ToList();
+        var updatedPrice = dbProducts.Select(x => objProducts.Single(y => x.IdProduct == y.guid).guantity * x.Price).Sum();
+
+        if(price != updatedPrice) {
+            return StatusCode(412, "The price has changed for one or more items. Please refresh the page.");
+        }
+
+        return Accepted(value: "The purchase request has been received, but it is only simulated, so the card hasn't been billed.");
+
     }
 
 }
--- SuperSold.UI.AspDotNet/Extensions/EnumerableExtensions.cs
+++ SuperSold.UI.AspDotNet/Extensions/EnumerableExtensions.cs
@@ -6,4 +6,15 @@ public static class EnumerableExtensions {
         return string.Join(separator, enumerable);
     }
 
+    public static async Task<List<T>> ToListAsync<T>(this IEnumerable<Task<T>> enumerable) {
+
+        var output = new List<T>();
+        foreach(var item in enumerable) {
+            output.Add(await item);
+        }
+
+        return output;
+
+    }
+
 }
