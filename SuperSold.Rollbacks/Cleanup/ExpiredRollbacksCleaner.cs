c11a3aab3d17696dace2d764a3858adc0cddf082
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-19 18:34:25 +0200

    (Untested) Implemented polling class to ensure that expired rollbacks are deleted.

---
+++ SuperSold.Rollbacks/Cleanup/ExpiredRollbacksCleaner.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperSold.Data.DBInteractions;
+
+namespace SuperSold.Rollbacks.Cleanup;
+public class ExpiredRollbacksCleaner : IAsyncDisposable {
+
+    private readonly IRollbackHandler _rollbackHandler;
+    private readonly TimeSpan _period;
+    private readonly PeriodicTimer _timer;
+    private readonly CancellationTokenSource _cts = new();
+    private readonly Task _cleanupLoop;
+
+    public ExpiredRollbacksCleaner(IRollbackHandler rollbackHandler, TimeSpan cleaningIntervalTime) {
+        _rollbackHandler = rollbackHandler;
+        _period = cleaningIntervalTime;
+        _timer = new(_period);
+        _cleanupLoop = Repeat();
+    }
+
+    private async Task Repeat() {
+        while(await _timer.WaitForNextTickAsync(_cts.Token) && !_cts.Token.IsCancellationRequested) {
+            await CleanExpired();
+        }
+    }
+
+    private async Task CleanExpired() {
+        await _rollbackHandler
+            .GetAllOlderThan(DateOnly.FromDateTime(DateTime.Now))
+            .ForEachAsync(x => _rollbackHandler.ExpireRollback(x.IdRollback));
+    }
+
+    public async ValueTask DisposeAsync() {
+        _cts.Cancel();
+        _timer.Dispose();
+        await _cleanupLoop;
+        GC.SuppressFinalize(this);
+    }
+}
