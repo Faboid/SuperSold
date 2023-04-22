4f712eab798f043a2b10cb03bfd54e5e6ca79bd6
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-22 15:23:31 +0200

    Implemented classes to interact with the html template and return it with inserted parameters.

---
+++ SuperSold.UI.AspDotNet/Services/EmailViewsBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace SuperSold.UI.AspDotNet.Services;
+
+public class EmailViewsBuilder : IEmailViewsBuilder {
+
+    private readonly IStaticHtmlTemplate _htmlTemplate;
+    public EmailViewsBuilder(IStaticHtmlTemplate htmlGetter) {
+        _htmlTemplate = htmlGetter;
+    }
+
+    public string BuildRollbackEmailHtml(string email, string rollbackLink, string expireDate) {
+        var page = _htmlTemplate.GetHtmlTemplate("RollbackEmailTemplate");
+
+        //replace values
+        page = page.Replace("{{email}}", email);
+        page = page.Replace("{{rollbackLink}}", rollbackLink);
+        page = page.Replace("{{expireDate}}", expireDate);
+
+        return page;
+    }
+
+}
---
+++ SuperSold.UI.AspDotNet/Services/IEmailViewsBuilder.cs
@@ -0,0 +1,5 @@
+﻿namespace SuperSold.UI.AspDotNet.Services;
+
+public interface IEmailViewsBuilder {
+    string BuildRollbackEmailHtml(string email, string rollbackLink, string expireDate);
+}
