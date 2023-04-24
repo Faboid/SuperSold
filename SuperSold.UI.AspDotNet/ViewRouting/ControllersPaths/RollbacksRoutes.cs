1d18629067a28d99ff7d9117fb0d2d3924b03ee4
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-24 20:30:53 +0200

    Implemented strongly typed routes.

--- SuperSold.UI.AspDotNet/ViewRouting/ControllersPaths/RollbacksRoutes.cs
+++ SuperSold.UI.AspDotNet/ViewRouting/ControllersPaths/RollbacksRoutes.cs
@@ -7,8 +7,12 @@ public class RollbacksRoutes : BaseRoutes
 
     public RollbacksRoutes(IUrlHelper urlHelper) : base(urlHelper, "Rollbacks") { }
 
+    public string? ForgotPasswordRequest() => BuildUrlToAction("ForgotPasswordRequest");
     public string? ForgotPassword() => BuildUrlToAction("ForgotPassword");
     public string? RollbackEmail() => BuildUrlToAction("RollbackEmail");
+
+    //used to send the link from outside the application's page
     public string? RollbackEmail(Guid userId, Guid token) => $"{BuildAbsoluteUrlToAction("RollbackEmail")}?userId={userId}&token={token}";
+    public string? ForgotPassword(Guid userId, Guid token) => $"{BuildAbsoluteUrlToAction("ForgotPassword")}?userId={userId}&token={token}";
 
 }
