d001365ce92c3fd6b47c3e82e36b20e8589f0c86
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-24 20:30:03 +0200

    Implemented building new email template.

--- SuperSold.UI.AspDotNet/Services/EmailViewsBuilder.cs
+++ SuperSold.UI.AspDotNet/Services/EmailViewsBuilder.cs
@@ -7,10 +7,18 @@ public class EmailViewsBuilder : IEmailViewsBuilder {
         _htmlTemplate = htmlGetter;
     }
 
+    public string BuildForgotPasswordEmailHtml(string rollbackLink, string expireDate) {
+        var page = _htmlTemplate.GetHtmlTemplate("ForgotPasswordTemplate");
+
+        page = page.Replace("{{rollbackLink}}", rollbackLink);
+        page = page.Replace("{{expireDate}}", expireDate);
+
+        return page;
+    }
+
     public string BuildRollbackEmailHtml(string email, string rollbackLink, string expireDate) {
         var page = _htmlTemplate.GetHtmlTemplate("RollbackEmailTemplate");
 
-        //replace values
         page = page.Replace("{{email}}", email);
         page = page.Replace("{{rollbackLink}}", rollbackLink);
         page = page.Replace("{{expireDate}}", expireDate);
