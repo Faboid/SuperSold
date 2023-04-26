c29c6bc626d1e45ad79f75342befdba26e22afe6
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-26 18:07:05 +0200

    Implemented interface and concrete classes for the repositories used to interact with roles and restrictions.

---
+++ SuperSold.Data/DBInteractions/IAccountRestrictionsHandler.cs
@@ -0,0 +1,14 @@
+﻿
+using OneOf;
+using OneOf.Types;
+using SuperSold.Data.Models;
+
+namespace SuperSold.Data.DBInteractions;
+
+public interface IAccountRestrictionsHandler {
+
+    Task<OneOf<Success, NotFound>> AddRestriction(Guid accountId, string restriction);
+    Task<OneOf<Success, NotFound>> DeleteRestriction(Guid accountId, string restriction);
+    IQueryable<AccountRestrictionModel> GetAccountRestrictions(Guid accountId);
+
+}
---
+++ SuperSold.Data/DBInteractions/IAccountRolesHandler.cs
@@ -0,0 +1,12 @@
+﻿using OneOf;
+using OneOf.Types;
+using SuperSold.Data.Models;
+
+namespace SuperSold.Data.DBInteractions;
+public interface IAccountRolesHandler {
+
+    Task<OneOf<Success, NotFound>> AddRole(Guid accountId, string role);
+    Task<OneOf<Success, NotFound>> DeleteRole(Guid accountId, string role);
+    IQueryable<AccountRoleModel> GetAccountRoles(Guid accountId);
+
+}
