f888e7142c414b6483a0be013458a9ad68740ae4
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-27 11:27:41 +0200

    Created AdminArea.

---
+++ SuperSold.UI.AspDotNet/Controllers/AdminAreaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SuperSold.UI.AspDotNet.Attributes;
+
+namespace SuperSold.UI.AspDotNet.Controllers;
+
+[Authorize]
+[AdminOnly]
+public class AdminAreaController : Controller {
+    public IActionResult Index() {
+        return View();
+    }
+}
