12dc84aca8203604537066af6053cc15bce1f6e5
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-27 11:27:28 +0200

    Set up attribute to check the presence of the admin role.

---
+++ SuperSold.UI.AspDotNet/Attributes/AdminOnlyAttribute.cs
@@ -0,0 +1,9 @@
+﻿using SuperSold.UI.AspDotNet.Constants;
+
+namespace SuperSold.UI.AspDotNet.Attributes;
+
+public class AdminOnlyAttribute : RequiresClaimsAttribute {
+    public AdminOnlyAttribute() : base(new RequiredClaim(Roles.Admin, "true")) { }
+}
+
+public record struct RequiredClaim(string Name, string Value);
---
+++ SuperSold.UI.AspDotNet/Attributes/RequiresClaimsAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SuperSold.UI.AspDotNet.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public abstract class RequiresClaimsAttribute : Attribute, IAuthorizationFilter {
+
+    private readonly RequiredClaim[] _requiredClaims;
+
+    protected RequiresClaimsAttribute(params RequiredClaim[] requiredClaims) {
+        _requiredClaims = requiredClaims;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context) {
+
+        if(!_requiredClaims.All(x => context.HttpContext.User.HasClaim(x.Name, x.Value))) {
+            context.Result = new ForbidResult();
+        }
+
+    }
+}
