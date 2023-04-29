8e0bb0c8961242b5f6641c4763293cf492c95f07
Faboid <72757782+Faboid@users.noreply.github.com>
2023-04-29 16:27:02 +0200

    Modified the account display view to show a more compacted, grid-like view.

--- SuperSold.UI.AspDotNet/ViewRouting/ViewComponents/AccountDisplayBuilder.cs
+++ SuperSold.UI.AspDotNet/ViewRouting/ViewComponents/AccountDisplayBuilder.cs
@@ -7,6 +7,6 @@ namespace SuperSold.UI.AspDotNet.ViewRouting.ViewComponents;
 public class AccountDisplayBuilder : BaseViewComponentBuilder {
     public AccountDisplayBuilder(IViewComponentHelper vcHelper) : base(vcHelper, "AccountDisplay") {}
 
-    public async Task<IHtmlContent> DisplayAsRow(AccountInfoModel account) => await Build(new { account, displayType = nameof(DisplayAsRow) });
+    public async Task<IHtmlContent> DisplayAsCell(AccountInfoModel account) => await Build(new { account, displayType = nameof(DisplayAsCell) });
 
 }
