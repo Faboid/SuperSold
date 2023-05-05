d5ac7fdc33a45778eb32fd59be28899dc97773bf
Faboid <72757782+Faboid@users.noreply.github.com>
2023-05-05 17:14:09 +0200

    Added dependency to MediatR.

--- SuperSold.UI.AspDotNet/Program.cs
+++ SuperSold.UI.AspDotNet/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddHostedService(s => {
 });
 
 builder.Services.AddAutoMapper();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 
 builder.Services.AddAntiforgery();
 var app = builder.Build();
