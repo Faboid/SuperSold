202b3746f6881ad7000cfecb7d6d3ad9b2f0a165
Faboid <72757782+Faboid@users.noreply.github.com>
2023-05-05 17:14:32 +0200

    Extracted HomeController logic to MediatR query and handlers.

---
+++ SuperSold.UI.AspDotNet/Handlers/Home/Queries/GetProductsInPage.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using SuperSold.Data.DBInteractions;
+using SuperSold.UI.AspDotNet.Extensions;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.Handlers.Home.Queries;
+
+public record GetProductsInPage(int Page, int PageLength) : IRequest<List<Product>>;
+
+public class GetProductsInPageHandler : IRequestHandler<GetProductsInPage, List<Product>> {
+
+    private readonly IProductsHandler _productsHandler;
+    private readonly IMapper _mapper;
+
+    public GetProductsInPageHandler(IProductsHandler productsHandler, IMapper mapper) {
+        _productsHandler = productsHandler;
+        _mapper = mapper;
+    }
+
+    public async Task<List<Product>> Handle(GetProductsInPage request, CancellationToken cancellationToken) {
+        return await _productsHandler
+            .QueryAllProducts()
+            .SkipToPage(request.Page, request.PageLength)
+            .ProjectTo<Product>(_mapper.ConfigurationProvider)
+            .ToListAsyncSafe();
+    }
+}
