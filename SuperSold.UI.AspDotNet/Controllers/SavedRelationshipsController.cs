8787674620f6cfa0a12f7d78a55c1454d84b1803
Faboid <72757782+Faboid@users.noreply.github.com>
2023-05-06 18:58:09 +0200

    Extracted SavedRelationships endpoint behavior with MediatR.

--- SuperSold.UI.AspDotNet/Controllers/SavedRelationshipsController.cs
+++ SuperSold.UI.AspDotNet/Controllers/SavedRelationshipsController.cs
@@ -1,26 +1,24 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using SuperSold.Data.DBInteractions;
-using SuperSold.UI.AspDotNet.Models;
+using SuperSold.UI.AspDotNet.Handlers.SavedRelationships.Commands;
+using SuperSold.UI.AspDotNet.Handlers.SavedRelationships.Queries;
 using SuperSold.UI.AspDotNet.ViewRouting;
 
 namespace SuperSold.UI.AspDotNet.Controllers;
 public class SavedRelationshipsController : Controller {
 
-    private readonly ISavedRelationshipsHandler _savedRelationshipsHandler;
-    private readonly IProductsHandler _productsHandler;
-    private readonly IMapper _mapper;
 
-    public SavedRelationshipsController(ISavedRelationshipsHandler savedRelationshipsHandler, IMapper mapper, IProductsHandler productsHandler) {
-        _savedRelationshipsHandler = savedRelationshipsHandler;
-        _mapper = mapper;
-        _productsHandler = productsHandler;
+    private readonly IMediator _mediator;
+
+    public SavedRelationshipsController(IMediator mediator) {
+        _mediator = mediator;
     }
 
     [HttpPost]
     public async Task<IActionResult> ModifyQuantity(Guid relationshipId, int quantity) {
 
-        var result = await _savedRelationshipsHandler.UpdateRelationshipQuantity(relationshipId, quantity);
+        var command = new ModifyRelationshipQuantityCommand(relationshipId, quantity);
+        var result = await _mediator.Send(command);
         return result.Match<IActionResult>(
             success => Ok(),
             notfound => NotFound()
@@ -31,22 +29,13 @@ public class SavedRelationshipsController : Controller {
     [HttpGet]
     public async Task<IActionResult> Get(Guid relationshipId, string itemRowFormat) {
 
-        var relationshipResult = await _savedRelationshipsHandler.GetRelationship(relationshipId);
-        if(relationshipResult.TryPickT1(out var _, out var relationship)) {
-            return NotFound();
-        }
+        var query = new GetProductWithRelationshipQuery(relationshipId);
+        var result = await _mediator.Send(query);
 
-        var productResult = await _productsHandler.GetProduct(relationship.IdProduct);
-        if(productResult.TryPickT1(out var _, out var product)) {
-            return NotFound();
-        }
-
-        var savedRelationship = new ProductWithSavedRelationship() { 
-            Product = _mapper.Map<Product>(product),
-            SavedRelationship = _mapper.Map<SavedRelationship>(relationship)
-        };
-
-        return this.RelationshipListPartialView(itemRowFormat, savedRelationship);
+        return result.Match(
+            product => this.RelationshipListPartialView(itemRowFormat, product),
+            notfound => NotFound()
+        );
 
     }
 
---
+++ SuperSold.UI.AspDotNet/Handlers/SavedRelationships/Queries/GetProductWithRelationshipQuery.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using SuperSold.Data.DBInteractions;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.Handlers.SavedRelationships.Queries;
+
+public record GetProductWithRelationshipQuery(Guid RelationshipId) : IRequest<OneOf<ProductWithSavedRelationship, NotFound>>;
+
+public class GetProductWithRelationshipHandler : IRequestHandler<GetProductWithRelationshipQuery, OneOf<ProductWithSavedRelationship, NotFound>> {
+
+    private readonly ISavedRelationshipsHandler _savedRelationshipsHandler;
+    private readonly IProductsHandler _productsHandler;
+    private readonly IMapper _mapper;
+
+    public GetProductWithRelationshipHandler(ISavedRelationshipsHandler savedRelationshipsHandler, IProductsHandler productsHandler, IMapper mapper) {
+        _savedRelationshipsHandler = savedRelationshipsHandler;
+        _productsHandler = productsHandler;
+        _mapper = mapper;
+    }
+
+    public async Task<OneOf<ProductWithSavedRelationship, NotFound>> Handle(GetProductWithRelationshipQuery request, CancellationToken cancellationToken) {
+        
+        var relationshipResult = await _savedRelationshipsHandler.GetRelationship(request.RelationshipId);
+        if(relationshipResult.TryPickT1(out var notfound, out var relationship)) {
+            return notfound;
+        }
+
+        var productResult = await _productsHandler.GetProduct(relationship.IdProduct);
+        if(productResult.TryPickT1(out notfound, out var product)) {
+            return notfound;
+        }
+
+        return new ProductWithSavedRelationship() {
+            Product = _mapper.Map<Product>(product),
+            SavedRelationship = _mapper.Map<SavedRelationship>(relationship)
+        };
+
+    }
+
+}
