82b35434795ba1ba4cfec862e0f273e3b43b4118
Faboid <72757782+Faboid@users.noreply.github.com>
2023-05-08 18:15:08 +0200

    Extracted ProfileController behavior to commands and queries with MediatR.

---
+++ SuperSold.UI.AspDotNet/Handlers/Profile/Queries/GetAccountDataQuery.cs
@@ -0,0 +1,20 @@
+﻿using SuperSold.Data.DBInteractions;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.Handlers.Profile.Queries;
+
+public record GetAccountDataQuery(Guid AccountId) : IRequest<OneOf<AccountInfoModel, NotFound>>;
+
+public class GetAccountDataHandler : IRequestHandler<GetAccountDataQuery, OneOf<AccountInfoModel, NotFound>> {
+
+    private readonly IAccountsHandler _accountHandler;
+
+    public GetAccountDataHandler(IAccountsHandler accountHandler) {
+        _accountHandler = accountHandler;
+    }
+
+    public async Task<OneOf<AccountInfoModel, NotFound>> Handle(GetAccountDataQuery request, CancellationToken cancellationToken) {
+        var user = await _accountHandler.GetAccountById(request.AccountId);
+        return user.MapT0(account => new AccountInfoModel() { Username = account.UserName, Email = account.Email });
+    }
+}
---
+++ SuperSold.UI.AspDotNet/Handlers/Profile/Queries/ValidateUsernameExistenceQuery.cs
@@ -0,0 +1,19 @@
+﻿using SuperSold.Data.DBInteractions;
+
+namespace SuperSold.UI.AspDotNet.Handlers.Profile.Queries;
+
+
+public record ValidateUsernameExistenceQuery(string Username) : IRequest<bool>;
+
+public class ValidateUsernameExistenceHandler : IRequestHandler<ValidateUsernameExistenceQuery, bool> {
+
+    private readonly IAccountsHandler _accountsHandler;
+
+    public ValidateUsernameExistenceHandler(IAccountsHandler accountsHandler) {
+        _accountsHandler = accountsHandler;
+    }
+
+    public async Task<bool> Handle(ValidateUsernameExistenceQuery request, CancellationToken cancellationToken) {
+        return await _accountsHandler.UserNameExists(request.Username);
+    }
+}
