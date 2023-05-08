43a8db00e07d1bcd96226ea70cf08e183934692c
Faboid <72757782+Faboid@users.noreply.github.com>
2023-05-08 18:59:33 +0200

    Extracted ProductsController behavior to commands and queries with MediatR.

---
+++ SuperSold.UI.AspDotNet/Handlers/Products/Commands/PublishProductCommand.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using SuperSold.Data.DBInteractions;
+using SuperSold.Data.Models;
+using SuperSold.Data.Models.ResponseTypes;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.Handlers.Products.Commands;
+
+
+public record PublishProductCommand(string SellerName, Product Product) : IRequest<OneOf<Success, AlreadyExists>>;
+
+public class PublishProductHandler : IRequestHandler<PublishProductCommand, OneOf<Success, AlreadyExists>> {
+
+    private readonly IProductsHandler _productsHandler;
+    private readonly IMapper _mapper;
+
+    public PublishProductHandler(IProductsHandler productsHandler, IMapper mapper) {
+        _productsHandler = productsHandler;
+        _mapper = mapper;
+    }
+
+    public async Task<OneOf<Success, AlreadyExists>> Handle(PublishProductCommand request, CancellationToken cancellationToken) {
+        return await _productsHandler.CreateProduct(_mapper.Map<ProductModel>(request.Product), request.SellerName);
+    }
+}
---
+++ SuperSold.UI.AspDotNet/Handlers/Products/Queries/GetProductQuery.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using SuperSold.Data.DBInteractions;
+using SuperSold.UI.AspDotNet.Models;
+
+namespace SuperSold.UI.AspDotNet.Handlers.Products.Queries;
+
+
+public record GetProductQuery(Guid ProductId) : IRequest<OneOf<Product, NotFound>>;
+
+public class GetProductHandler : IRequestHandler<GetProductQuery, OneOf<Product, NotFound>> {
+
+    private readonly IProductsHandler _productsHandler;
+    private readonly IMapper _mapper;
+
+    public GetProductHandler(IProductsHandler productsHandler, IMapper mapper) {
+        _productsHandler = productsHandler;
+        _mapper = mapper;
+    }
+
+    public async Task<OneOf<Product, NotFound>> Handle(GetProductQuery request, CancellationToken cancellationToken) {
+
+        var result = await _productsHandler.GetProduct(request.ProductId);
+        return result.MapT0(_mapper.Map<Product>);
+
+    }
+}
